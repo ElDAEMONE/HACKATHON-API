2be57129aed65b1dd291e3c78ec33bb9163d95fe
Ranmina Dolawatta <it21101274@my.sliit.lk>
2026-07-01 18:56:45 +0530

    feat: get user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  // Get /user
+  @Get()
+  getUsers(){
+    return[
+      { id: 1, name: 'John Doe' },
+      { id: 2, name: 'Jon jon' }
+    ];
+  }
+}
