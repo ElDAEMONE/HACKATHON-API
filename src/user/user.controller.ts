1ef86b75f8bd4f189605ed653e90fa7740275838
Ranmina Dolawatta <it21101274@my.sliit.lk>
2026-07-04 16:04:14 +0530

    feat:controllers

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   // Get /user
   @Get()
-  getUsers(){
-    return[
-      { id: 1, name: 'John Doe' },
+  getUsers(@Query('name') name: string) {
+    const users = [
+      { id: 1, name:  'John Doe' },
       { id: 2, name: 'Jon jon' }
     ];
+  
+    if (name) {
+      return users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
+    }
+    return users;
   }
 }
